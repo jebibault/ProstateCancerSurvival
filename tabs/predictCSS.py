8fca3b2b24692c9849d85e667c07499359715101
Jean-Emmanuel Bibault <jebibault@gmail.com>
2022-08-04 11:41:04 +0200

    Update predictCSS.py

--- tabs/predictCSS.py
+++ tabs/predictCSS.py
@@ -1,7 +1,7 @@
 from plotly.tools import mpl_to_plotly
 from dash.dependencies import Input, Output
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 import pickle
 import numpy as np
 import pandas as pd
